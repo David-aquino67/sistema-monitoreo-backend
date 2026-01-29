69a13b04cfe18ec73710e5fb75d3cdb641cab2a6
Carlos <carlos.aquinore@alumno.buap.mx>
2026-01-29 05:52:29 -0600

    definicion de model y modificacion de la generacion de datos

--- simf-back/app/Models/Servidor.php
+++ simf-back/app/Models/Servidor.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Servidor extends Model
 {
-    //
+    protected $table = 'servidores';
+    protected $fillable = [
+        'titulo',
+        'ubicacion',
+        'ip',
+        'estado',
+        'tiempoActividad',
+        'latencia',
+        'ultimoPing',
+        'permisos'
+    ];
+    protected $casts = [
+        'permisos' => 'array',
+        'ultimoPing' => 'datetime',
+    ];
 }
