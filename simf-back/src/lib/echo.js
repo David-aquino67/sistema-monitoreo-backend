0ec72af52166a8e4fd31cc076d87f364f83bfc81
Carlos <carlos.aquinore@alumno.buap.mx>
2026-02-03 11:56:02 -0600

    configuracion de websocket puerto echo.js

--- simf-back/src/lib/echo.js
+++ simf-back/src/lib/echo.js
@@ -7,7 +7,7 @@ export const echo = new Echo({
 broadcaster: 'reverb',
 key: import.meta.env.VITE_REVERB_APP_KEY,
 wsHost: import.meta.env.VITE_REVERB_HOST,
-wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
 forceTLS: false,
 enabledTransports: ['ws', 'wss'],
 });
