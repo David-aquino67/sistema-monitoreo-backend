7c211de47b4cd590bad6bca9e70fb9cc1f0f071a
Carlos <carlos.aquinore@alumno.buap.mx>
2026-02-27 13:30:18 -0600

    importacion librerias a serversatuscambio.php

--- app/Events/ServerStatusCambio.php
+++ app/Events/ServerStatusCambio.php
@@ -1,5 +1,13 @@
 <?php
-class ServerStatusUpdated implements ShouldBroadcast
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ServerStatusCambio implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
