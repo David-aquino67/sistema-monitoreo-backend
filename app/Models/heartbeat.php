e4899366a08e19332b931e86eb45fe6f49119fa7
Carlos <carlos.aquinore@alumno.buap.mx>
2026-03-03 10:31:58 -0600

    cambio en el modelo heartbeat

--- app/Models/heartbeat.php
+++ app/Models/heartbeat.php
@@ -1,19 +1,17 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Heartbeat extends Model
 {
-    private $table = "heartbeat";
-    private $primaryKey = "id";
-    private $monitor_id = "monitor_id";
-    private $status = "status";
-    private $msg = "msg";
-    private $time = "time";
-    private $ping = "ping";
-
-    protected $connection = 'kuma';
+    
+    protected $connection = 'kuma'; 
+    protected $table = "heartbeat";
+    protected $primaryKey = "id";
     public $timestamps = false;
+
     public function monitor()
     {
         return $this->belongsTo(Monitor::class, 'monitor_id', 'id');
