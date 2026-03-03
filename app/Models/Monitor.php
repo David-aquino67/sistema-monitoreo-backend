e42bd72fb461e19255477ab8d13f91b8046087e0
Carlos <carlos.aquinore@alumno.buap.mx>
2026-03-03 10:51:13 -0600

    cambio en los modelos de monitores

--- app/Models/Monitor.php
+++ app/Models/Monitor.php
@@ -1,14 +1,12 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
-class Monitor extends Model
+class Monitor extends Model 
 {
-    protected $table = 'monitores_servidores';
-    protected $primaryKey = 'REGISTRO_id';
-    public $timestamps = false;
-    protected $fillable = [
-        'FK_id_unidad',
-        'FK_id_monitor_kuma'
-    ];
+    protected $connection = 'kuma';
+    protected $table = "monitor";
+    protected $primaryKey = "id";
+    public $timestamps = false; 
 }
--- app/Models/MonitorServidor.php
+++ app/Models/MonitorServidor.php
@@ -8,7 +8,8 @@ class MonitorServidor extends Model
 {
     protected $table = "monitores_servidores";
     protected $primaryKey = "REGISTRO_id";
- 
-	const CREATED_AT = 'REGISTRO_fecha_creacion';
-	const UPDATED_AT = 'REGISTRO_fecha_ultimo_cambio';
+    
+    public $timestamps = true;
+    const CREATED_AT = 'REGISTRO_fecha_creacion';
+    const UPDATED_AT = 'REGISTRO_fecha_ultimo_cambio';
 }
