83723a5653c6042c1f30416869d6b3094d744d09
Carlos <carlos.aquinore@alumno.buap.mx>
2026-03-18 06:33:44 -0600

    refactor: ServidorController.php

--- app/Http/Controllers/ServidorController.php
+++ app/Http/Controllers/ServidorController.php
@@ -11,29 +11,6 @@ class ServidorController extends Controller
 {
     public function index()
     {
-        // Traemos todos los servidores mapeados desde SQL Server
-        // Seleccionamos los campos que React necesita para pintar las tarjetas
-
-        $servidores = MonitorServidor::all()->map(function($item) {
-            return [
-                'id' => $item->REGISTRO_id, // El ID interno del SIBOP
-                'unidad_id' => $item->FK_id_unidad,
-                'online' => false, // Estado inicial por defecto
-                'latencia' => 0    // Latencia inicial
-            ];
-        });
-
-        $ids = $servidores->pluck('unidad_id')->toArray();
-        $unidades = Sibop::unidades(env('SIBOP_API_TOKEN'), $ids);
-
-        $servidores = $servidores->map(function($servidor) use ($unidades) {
-            $unidad_sibop = array_filter($unidades, function($unidad) use ($servidor) {
-                return ((int) $unidad['REGISTRO_id']) === ((int) $servidor['unidad_id']);
-            });
-            $servidor['nombre'] = $unidad_sibop ? reset($unidad_sibop)['descripcion'] : 'Desconocido';
-            return $servidor; 
-        });
-
-        return response()->json($servidores);
+        /return response()->json($statusService->getStatusFull());
     }
 }
