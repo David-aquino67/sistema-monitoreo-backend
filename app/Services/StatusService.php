5e584120fba329443ea6d7099e2e903046fbb28c
Carlos <carlos.aquinore@alumno.buap.mx>
2026-03-19 11:39:54 -0600

    refactor: cache para nombres de unidad

--- app/Services/StatusService.php
+++ app/Services/StatusService.php
@@ -9,27 +9,28 @@ use Illuminate\Support\Facades\Cache;
 
 class StatusService
 {
+    private $refresh_cahe = 5184000;
+
     public function getStatusFull()
     {
-        return Cache::remember('servidores_status_full', 300, function () {
-            $mapeos = MonitorServidor::all();
-            $ids = $mapeos->pluck('FK_id_unidad')->toArray();
-            $unidadesSibop = Sibop::unidades(env('SIBOP_API_TOKEN'), $ids); // usar para cache 
-            return $mapeos->map(function ($item) use ($unidadesSibop) {
-                $unidadData = collect($unidadesSibop)->firstWhere('REGISTRO_id', $item->FK_id_unidad);
-                $ultimoHb = Heartbeat::where('monitor_id', $item->FK_id_monitor_kuma)
-                    ->orderBy('time', 'desc')
-                    ->first();
-
-                return [
-                    'id'        => $item->REGISTRO_id,
-                    'unidad_id' => $item->FK_id_unidad,
-                    'nombre'    => $unidadData['descripcion'] ?? 'Unidad Desconocida',
-                    'online'    => $ultimoHb ? (bool)$ultimoHb->status : false,
-                    'latencia'  => $ultimoHb ? $ultimoHb->ping : 0,
-                    'fecha'     => $ultimoHb ? $ultimoHb->time : null,
-                ];
-            });
+        $mapeos = MonitorServidor::all();
+        $ids = $mapeos->pluck('FK_id_unidad')->toArray();
+        $unidadesSibop = Cache::remember('catalogo_unidades_sibop', $this->refresh_cahe, function () use ($ids) {
+            return Sibop::unidades(env('SIBOP_API_TOKEN'), $ids);
+        });
+        return $mapeos->map(function ($item) use ($unidadesSibop) {
+            $unidadData = collect($unidadesSibop)->firstWhere('REGISTRO_id', $item->FK_id_unidad);
+            $ultimoHb = Heartbeat::where('monitor_id', $item->FK_id_monitor_kuma)
+                ->orderBy('time', 'desc')
+                ->first();
+            return [
+                'id'        => $item->REGISTRO_id,
+                'unidad_id' => $item->FK_id_unidad,
+                'nombre'    => $unidadData['descripcion'] ?? 'Unidad Desconocida',
+                'online'    => $ultimoHb ? (bool)$ultimoHb->status : false,
+                'latencia'  => $ultimoHb ? $ultimoHb->ping : 0,
+                'fecha'     => $ultimoHb ? $ultimoHb->time : null,
+            ];
         });
     }
 }
