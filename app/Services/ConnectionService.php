e832594a3b76cb94162c235fd23ff19aa2bb558e
Carlos <carlos.aquinore@alumno.buap.mx>
2026-03-25 14:01:56 -0600

    refactor: funcion getCredentialsByUnidad

--- app/Services/ConnectionService.php
+++ app/Services/ConnectionService.php
@@ -9,19 +9,23 @@ class ConnectionService
     {
         $this->encryption = $encryption;
     }
-    public function getCredentialsByUnidad($unidadId)
+   public function getCredentialsByUnidad($unidadId)
     {
-        $conexion = DB::table('monitor_conexion')
-            ->join('tblCons', 'monitor_conexion.constant_id', '=', 'tblCons.ConstantID')
-            ->where('monitor_conexion.unidad_id', $unidadId)
-            ->select('tblCons.Username', 'tblCons.Password', 'tblCons.Hostname', 'tblCons.Port')
-            ->first();
-        if (!$conexion) return null;
-        return [
-            'host' => $conexion->Hostname,
-            'user' => $conexion->Username,
-            'port' => $conexion->Port,
-            'pass' => $this->encryption->decryptMRemote($conexion->Password)
-        ];
-    }
+    $conexion = DB::table('monitor_conexion')
+        ->join('tblCons', function($join) {
+            $join->on('monitor_conexion.constant_id', '=', DB::raw('tblCons.ConstantID COLLATE SQL_Latin1_General_CP1_CI_AS'));
+        })
+        ->where('monitor_conexion.unidad_id', $unidadId)
+        ->select('tblCons.Username', 'tblCons.Password', 'tblCons.Hostname', 'tblCons.Port')
+        ->first();
+
+    if (!$conexion) return null;
+
+    return [
+        'host' => $conexion->Hostname,
+        'user' => $conexion->Username,
+        'port' => $conexion->Port,
+        'pass' => $this->encryption->decryptMRemote($conexion->Password)
+    ];
+}
 }
