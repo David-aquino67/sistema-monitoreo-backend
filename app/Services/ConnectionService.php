31f189c641e1add2e6bc37566511a1aaa98248ed
Carlos <carlos.aquinore@alumno.buap.mx>
2026-03-31 13:34:32 -0600

    feat: comunicacion con  la bd, llamada a la funcion connectionservice.php

--- app/Services/ConnectionService.php
+++ app/Services/ConnectionService.php
@@ -1,31 +1,37 @@
 <?php
+
 namespace App\Services;
+
 use Illuminate\Support\Facades\DB;
 
 class ConnectionService
 {
     protected $encryption;
+
     public function __construct(EncryptionService $encryption)
     {
         $this->encryption = $encryption;
     }
-   public function getCredentialsByUnidad($unidadId)
+
+    public function getCredentialsByUnidad(int $unidadId): ?array
     {
-    $conexion = DB::table('monitor_conexion')
-        ->join('tblCons', function($join) {
-            $join->on('monitor_conexion.constant_id', '=', DB::raw('tblCons.ConstantID COLLATE SQL_Latin1_General_CP1_CI_AS'));
-        })
-        ->where('monitor_conexion.unidad_id', $unidadId)
-        ->select('tblCons.Username', 'tblCons.Password', 'tblCons.Hostname', 'tblCons.Port')
-        ->first();
+        $conexion = DB::table('monitor_conexion')
+            ->join('tblCons', function ($join) {
+                $join->on('monitor_conexion.constant_id', '=', DB::raw('tblCons.ConstantID COLLATE SQL_Latin1_General_CP1_CI_AS'));
+            })
+            ->where('monitor_conexion.unidad_id', $unidadId)
+            ->select('tblCons.Username', 'tblCons.Password', 'tblCons.Hostname', 'tblCons.Port')
+            ->first();
 
-    if (!$conexion) return null;
+        if (!$conexion) {
+            return null;
+        }
 
-    return [
-        'host' => $conexion->Hostname,
-        'user' => $conexion->Username,
-        'port' => $conexion->Port,
-        'pass' => $this->encryption->decryptMRemote($conexion->Password)
-    ];
-}
+        return [
+            'host' => $conexion->Hostname,
+            'user' => $conexion->Username,
+            'port' => $conexion->Port,
+            'pass' => $this->encryption->decryptMRemote($conexion->Password),
+        ];
+    }
 }
