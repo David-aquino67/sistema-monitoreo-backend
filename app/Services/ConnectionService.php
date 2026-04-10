bb029901aa1e770ef03506f91c6d1982ff7ad6a4
Carlos <carlos.aquinore@alumno.buap.mx>
2026-04-10 12:07:46 -0600

    chore:implementacion del patron funtional descomposition en  connectionservice

--- app/Services/ConnectionService.php
+++ app/Services/ConnectionService.php
@@ -15,18 +15,30 @@ class ConnectionService
 
     public function getCredentialsByUnidad(int $unidadId): ?array
     {
-        $conexion = DB::table('monitor_conexion')
+       $conexionRaw = $this->encontrarConnectionData($unidadId);
+
+        if (!$conexionRaw) {
+            return null;
+        }
+
+        return $this->formatoDecryptCredentials($conexionRaw);
+    }
+    private function encontrarConnectionData(int $unidadId)
+    {
+        return DB::table('monitor_conexion')
             ->join('tblCons', function ($join) {
-                $join->on('monitor_conexion.constant_id', '=', DB::raw('tblCons.ConstantID COLLATE SQL_Latin1_General_CP1_CI_AS'));
+                $join->on(
+                    'monitor_conexion.constant_id', 
+                    '=', 
+                    DB::raw('tblCons.ConstantID COLLATE SQL_Latin1_General_CP1_CI_AS')
+                );
             })
             ->where('monitor_conexion.unidad_id', $unidadId)
             ->select('tblCons.Username', 'tblCons.Password', 'tblCons.Hostname', 'tblCons.Port')
             ->first();
-
-        if (!$conexion) {
-            return null;
-        }
-
+    }
+    private function formatoDecryptCredentials($conexion): array
+    {
         return [
             'host' => $conexion->Hostname,
             'user' => $conexion->Username,
