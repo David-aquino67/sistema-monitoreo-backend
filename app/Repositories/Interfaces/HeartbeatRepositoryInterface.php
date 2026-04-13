9e61aae2ca3abcc4b52e14aa18838e2dad21d110
Carlos <carlos.aquinore@alumno.buap.mx>
2026-04-13 02:05:52 -0600

    feat:definicion de interfaces

--- app/Repositories/Interfaces/HeartbeatRepositoryInterface.php
+++ app/Repositories/Interfaces/HeartbeatRepositoryInterface.php
@@ -6,4 +6,5 @@ use App\Models\Heartbeat;
 interface HeartbeatRepositoryInterface
 {
     public function obtenerUltimoHeartbeat(int $monitor_Id): ?Heartbeat;
+    public function obtenerUltimoDeMonitorId(int $monitor_Id): ?Heartbeat;
 }
