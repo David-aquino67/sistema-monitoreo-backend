3918a56c84571f77fdeec6377d10cabef36af4ac
Luis Alfredo Monroy Villegas <lur0y@hotmail.com>
2026-04-21 11:02:54 -0600

    Se agrega creación de servidor de web sockets a comando dev

--- app/Console/Commands/Dev.php
+++ app/Console/Commands/Dev.php
@@ -8,11 +8,14 @@ use App\Services\GlobalService;
 class Dev extends Command
 {
     protected $signature = 'dev';
-    protected $description = 'Levanta servidor de desarrollo y la cola de trabajo';
+    protected $description = 'Levanta servidor de desarrollo, cola de trabajo y websockets';
     public function handle(GlobalService $globalService)
     {
 		$host = env('DEV_HOST', '127.0.0.1');
 		$port = env('DEV_PORT', '8000');
+		$reverbHost = env('REVERB_SERVER_HOST', env('REVERB_HOST', '0.0.0.0'));
+		$reverbPort = env('REVERB_SERVER_PORT', env('REVERB_PORT', '8080'));
+		$reverbHostname = env('REVERB_HOST', $reverbHost);
 
 		// Eliminamos la cache
 		$this->info("Limpiando cache...");
@@ -21,6 +24,9 @@ class Dev extends Command
 		// Lanzamos los procesos asíncronos en ventanas nuevas
 		$this->info("Iniciando Worker de colas...");
 		pclose(popen("start php artisan queue:listen > NUL", "r"));
+
+		$this->info("Iniciando WebSockets (Reverb) en {$reverbHost}:{$reverbPort}...");
+		pclose(popen("start php artisan reverb:start --host={$reverbHost} --port={$reverbPort} --hostname={$reverbHostname} > NUL", "r"));
 
 		// Lanzamos el servidor de desarrollo
 		$this->info("Iniciando servidor en http://{$host}:{$port}");
