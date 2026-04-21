d84becdb52e2a70402f16aa1e8c9b64b7a05d9cd
Luis Alfredo Monroy Villegas <lur0y@hotmail.com>
2026-04-21 11:24:06 -0600

    Ahora el comando Dev tambien lanza el scheduler

--- app/Console/Commands/Dev.php
+++ app/Console/Commands/Dev.php
@@ -8,14 +8,13 @@ use App\Services\GlobalService;
 class Dev extends Command
 {
     protected $signature = 'dev';
-    protected $description = 'Levanta servidor de desarrollo, cola de trabajo y websockets';
+	protected $description = 'Levanta servidor de desarrollo, cola de trabajo, scheduler y websockets';
     public function handle(GlobalService $globalService)
     {
 		$host = env('DEV_HOST', '127.0.0.1');
 		$port = env('DEV_PORT', '8000');
 		$reverbHost = env('REVERB_SERVER_HOST', env('REVERB_HOST', '0.0.0.0'));
 		$reverbPort = env('REVERB_SERVER_PORT', env('REVERB_PORT', '8080'));
-		$reverbHostname = env('REVERB_HOST', $reverbHost);
 
 		// Eliminamos la cache
 		$this->info("Limpiando cache...");
@@ -25,8 +24,13 @@ class Dev extends Command
 		$this->info("Iniciando Worker de colas...");
 		pclose(popen("start php artisan queue:listen > NUL", "r"));
 
+		// Lanzamos el servidor de WebSockets (Reverb)
 		$this->info("Iniciando WebSockets (Reverb) en {$reverbHost}:{$reverbPort}...");
-		pclose(popen("start php artisan reverb:start --host={$reverbHost} --port={$reverbPort} --hostname={$reverbHostname} > NUL", "r"));
+		pclose(popen("start php artisan reverb:start --host={$reverbHost} --port={$reverbPort} --debug > NUL 2>&1", "r"));
+
+		// Lanzamos el scheduler
+		$this->info("Iniciando scheduler (schedule:work)...");
+		pclose(popen("start php artisan schedule:work > NUL 2>&1", "r"));
 
 		// Lanzamos el servidor de desarrollo
 		$this->info("Iniciando servidor en http://{$host}:{$port}");
